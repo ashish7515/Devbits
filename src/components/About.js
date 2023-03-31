69822355fc37cfec623e271b8c2672ec39419174
ashish7515 <ashish.kumar.ece21@itbhu.ac.in>
2023-03-31 14:15:07 +0530

    update 6

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
                     <div className="right">
                         Welcome to Devfolio, the go-to source for all things related to stocks and finance. Our mission is to empower individuals with the knowledge and tools they need to make informed financial decisions.
                              <br /> Our team of experienced professionals has a combined experience of 3 years in the industry. We pride ourselves on our expertise and dedication to providing the highest quality services to our clients. <br />  <br />
-                             Devfolio was founded in 2020 by MR. S. K. Raman, who saw a need for a website that provided accurate, up-to-date financial information in an easy-to-understand format. Since then, we've grown to become one of the leading resources for anyone looking to learn more about investing, stocks, and finance.
+                             Devfolio was founded in 2020 by Ashish, Nisarg, Aryan who saw a need for a website that provided accurate, up-to-date financial information in an easy-to-understand format. Since then, we've grown to become one of the leading resources for anyone looking to learn more about investing, stocks, and finance.
                             <br /><br /> At Devfolio, we believe in transparency, honesty, and integrity. We're committed to providing our visitors with unbiased, objective information, and we never accept payment in exchange for positive reviews or endorsements.
                     </div>
                 </div>
